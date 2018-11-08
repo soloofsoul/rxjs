7a45d6337794a116aed394adc746f3bc97ed9c8a
Grygorii Gavaleshko <grygoriy@gavaleshko.org.ua>
2018-11-08 13:30:38 +0100

    Added updates for Observable

--- src/Observable/observableExample.js
+++ src/Observable/observableExample.js
@@ -1,13 +1,17 @@
 import { Observable } from 'rxjs';
 
 const observerFn = observer => {
-  observer.next(1);
-  observer.next(2);
-  observer.next(3);
-  setTimeout(() => {
-    observer.next(4);
-    observer.complete();
-  }, 1000);
+  try {
+    observer.next(1);
+    observer.next(2);
+    observer.next(3);
+    setTimeout(() => {
+      observer.next(4);
+      observer.complete();
+    }, 1000);
+  } catch (err) {
+    observer.error(err);
+  }
 };
 
 const observableExample = Observable.create(observerFn);
